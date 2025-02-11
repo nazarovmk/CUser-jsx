e5149edadcc8284ab905a0211ddb531a476824b9
Muhammadnazar <muhammadnazarnazarov25@gmail.com>
2025-02-11 16:25:43 +0500

    CUser jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+    <div className="footer">
+        <div className="footer-container container">
+            <p>All Right Reserved. <a href="https://t.me/@nazarov_mk">Telegram</a></p>
+        </div>
+    </div>
+    )
+}
+
+export default Footer;
